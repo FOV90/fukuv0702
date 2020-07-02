9205537833a6f90ec5a02b981bb92b8d07efb3e9
Aleksandra <shkolnoe18@gmail.com>
2020-07-02 10:16:07 +0900

    a

--- fukuv0702/Form1.cs
+++ fukuv0702/Form1.cs
@@ -16,5 +16,20 @@ namespace fukuv0702
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Visible = false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button2.Text = "押された";
+        }
     }
 }
